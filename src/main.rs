3371429fae5a98aa945a5ea2b1e39cda197177af
alexborja <gersonperezgerder@gmail.com>
2023-06-27 19:33:50 -0600

    init test

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,27 @@
 #![no_std] // no standar library
 #![no_main] // no entry point
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello world";
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-   /* let vga_buffer = 0xb8000 as *mut u8;
-
-    for(i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-
-        }
-    }*/
-
-    // the linker look for _start
-    //vga_buffer::print_something;
-    //vga_buffer::WRITER.lock().write_str("Hello String").unwrap();
-    //write!(vga_buffer::WRITER.lock(), ", some numbers: {} {} ", 42, 1.0/3.0).unwrap();
-
     println!("Hello world{}", "!");
+    
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
@@ -35,3 +32,15 @@ fn panic(_info: &PanicInfo) -> ! {
 
     loop {}
 }
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+
+
+
+
