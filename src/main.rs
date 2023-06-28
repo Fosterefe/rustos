164f29345ba03bf73dbb8b0c52c47337798ee65d
alexborja <gersonperezgerder@gmail.com>
2023-06-27 18:39:24 -0600

    vga applied

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 #![no_main] // no entry point
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 static HELLO: &[u8] = b"Hello world";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    use core::fmt::Write;
+   /* let vga_buffer = 0xb8000 as *mut u8;
 
     for(i, &byte) in HELLO.iter().enumerate() {
         unsafe {
@@ -15,9 +17,13 @@ pub extern "C" fn _start() -> ! {
             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
 
         }
-    }
+    }*/
 
     // the linker look for _start
+    vga_buffer::print_something;
+    vga_buffer::WRITER.lock().write_str("Hello String").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {} ", 42, 1.0/3.0).unwrap();
+
     loop {}
 }
 
